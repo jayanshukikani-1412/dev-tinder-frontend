4f4c9dff77b70750df92206fee9da3ac46da44af
jayanshukikani-1412 <jayanshukikani14@gmail.com>
2026-03-10 19:54:41 +0530

    Add Footer component and integrate it into the Body component

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./navbar";
 import { Outlet } from "react-router-dom";
+import Footer from "./footer";
 
 const Body = () => {
   return (
     <div>
       <Navbar />
       <Outlet />
+      <Footer />
     </div>
   );
 };
