d013a5a9edd8025f877aa149688c2c50216152b3
jayanshukikani-1412 <jayanshukikani14@gmail.com>
2026-03-11 10:26:19 +0530

    Enhance Login component to support user signup functionality, including first and last name fields. Refactor form submission logic to handle both login and signup, and update UI to toggle between the two modes.

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -2,17 +2,20 @@ import axios from "axios";
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addUser } from "../utils/user-slice";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { BASE_URL } from "../utils/constant";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [isLoginForm, setIsLoginForm] = useState(true);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
-  const submitHandler = async (e) => {
+  const submitLoginHandler = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.post(
@@ -31,15 +34,62 @@ const Login = () => {
     }
   };
 
+  const submitSignupHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        BASE_URL + "/signup",
+        {
+          firstName: firstName,
+          lastName: lastName,
+          emailId: email.trim().toLowerCase(),
+          password: password,
+        },
+        { withCredentials: true },
+      );
+      dispatch(addUser(response?.data?.data));
+      navigate("/profile");
+      return response;
+    } catch (error) {
+      setError(error.response?.data || "something went wrong");
+    }
+  };
+
   return (
     <div className="flex justify-center my-10">
       <div className="card bg-base-300 w-96 shadow-sm">
         <div className="card-body">
           <h2 className="card-title text-2xl text-center justify-center">
-            Login
+            {isLoginForm ? "Login" : "Signup"}
           </h2>
-          <form onSubmit={submitHandler}>
+          <form
+            onSubmit={isLoginForm ? submitLoginHandler : submitSignupHandler}
+          >
             <div>
+              {!isLoginForm && (
+                <fieldset className="fieldset p-2">
+                  <legend className="fieldset-legend">First Name</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={firstName}
+                    onChange={(e) => setFirstName(e.target.value.trim())}
+                  />
+                </fieldset>
+              )}
+
+              {!isLoginForm && (
+                <fieldset className="fieldset p-2">
+                  <legend className="fieldset-legend">Last Name</legend>
+                  <input
+                    type="text"
+                    className="input"
+                    value={lastName}
+                    onChange={(e) => setLastName(e.target.value.trim())}
+                  />
+                </fieldset>
+              )}
+
               <fieldset className="fieldset p-2">
                 <legend className="fieldset-legend">Email ID</legend>
                 <input
@@ -63,15 +113,26 @@ const Login = () => {
               </fieldset>
             </div>
             {error && <p className="text-red-400 my-2 mb-4">{error}</p>}
-            <div className="card-actions justify-center">
+            <div className="card-actions justify-center mt-3">
               <button
                 className="btn btn-primary"
                 type="submit"
               >
-                Login
+                {isLoginForm ? "Login" : "Signup"}
               </button>
             </div>
           </form>
+          <p className="text-center text-sm text-gray-500 mt-5">
+            {isLoginForm
+              ? "Don't have an account? "
+              : "Already have an account? "}
+            <Link
+              onClick={() => setIsLoginForm(!isLoginForm)}
+              className="text-blue-500 cursor-pointer"
+            >
+              {isLoginForm ? "Signup here" : "Login here"}
+            </Link>
+          </p>
         </div>
       </div>
     </div>
